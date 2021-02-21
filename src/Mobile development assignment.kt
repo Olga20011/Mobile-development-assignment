eff43cfe5ebf332f5f1aecc8aa85034e97fb672d
olga20011 <olgaestherakello@gmail.com>
2021-02-21 09:34:20 +0300

    Mobile development assignment

---
+++ src/Mobile development assignment.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val studentName="Akello Olga"
+    print(studentName)
+    var studentAge=19
+    print (studentAge)
+    var studentWeight=67.78906
+    print(studentWeight)
+    val phoneNumber="0757351811"
+    print(phoneNumber)
+    val citizen=("false")
+    println(citizen)
+    var student=arrayOf("Akello Olga",19,67.78906,"0757351811","false")
+    print (studentName)
+
+
+
+
+
+
+}
+
+
